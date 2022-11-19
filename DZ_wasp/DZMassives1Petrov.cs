9ff93fb14449011d5fd9f71e0ba14aefb347e309
alexishotab <lesha-petrow-2004@yandex.ru>
2022-11-19 17:57:31 +0300

    Update DZ with massives

---
+++ DZ_wasp/DZMassives1Petrov.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+namespace DZ4Csharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] numbers = new int[a];
+            double answer = 0;
+            for (int i = 0; i < a; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                numbers[i] = x;
+            }
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int cash=numbers[j];
+                        numbers[j] = numbers[j+1];
+                        numbers[j+1] = cash;
+                    }
+                }
+            }
+            for (int i = 0; i < a; i++) 
+                Console.WriteLine(numbers[i]);
+
+            //string binary = Convert.ToString(answer, 2);
+
+        }
+    }
+}
---
+++ DZ_wasp/DZMassives2Petrov.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] numbers = new int[a];
+            double answer = 0;
+            for (int i = 0; i < a; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                numbers[i] = x;
+            }
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < numbers.Length-1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int cash = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = cash;
+                    }
+                }
+            }
+            for (int i = 0; i < a; i++)
+                if (i==k-1)
+                    Console.Write(numbers[i]);
+
+            //string binary = Convert.ToString(answer, 2);
+
+        }
+    }
+}
